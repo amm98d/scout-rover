d99b645ba1f2b4a6c7897b71ca5045b5295f1080
amm98d <ahmedmustafamalik98@gmail.com>
2020-12-09 02:50:12 +0500

    [Refactor][Rover] rover_stream.py file in Working Condition.

--- rover/rover_stream.py
+++ rover/rover_stream.py
@@ -1,37 +1,23 @@
-# import the necessary packages
-from imutils.video import VideoStream
-import imagezmq
-import argparse
-import socket
+import sys
+sys.path.append("../common/")
+from NumpySocket import NumpySocket
+
 import time
-import cv2 as cv2
+import numpy as np
+from freenect import sync_get_depth as get_depth, sync_get_video as get_video
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-s", "--server-ip", required=True, help="ip address of the server to which the client will connect")
-args = vars(ap.parse_args())
-# initialize the ImageSender object with the socket address of the
-# server
-sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(args["server_ip"]))
-# get the host name, initialize the video stream, and allow the
-# camera sensor to warmup
-rpiName = socket.gethostname()
-#vs = VideoStream(usePiCamera=True).start()
-vs = VideoStream(src=0).start()
-#vs=cv2.VideoCapture(0)
-time.sleep(2.0)
-i=0
+host_ip = '192.168.100.14'
+
+npSocket = NumpySocket()
+npSocket.startServer(host_ip, 9999)
+
 while True:
-    # read the frame from the camera and send it to the server
-    frame=vs.read()
-    #cv2.imwrite("Testing" + str(i) +'.jpg' , frame)
-    time.sleep(0.016)
-    sender.send_image(rpiName , frame)
-    i+=1
-# while True:
-# 	# read the frame from the camera and send it to the server
-# 	frame = vs.read()
-#     cv2.imwrite("fDetector"+'.jpg',frame)
-#     sender.send_image(rpiName, frame)
+	frame, _ = get_video()
+	print(len(frame),len(frame[0]),len(frame[0][0]))
+	time.sleep(0.016)
+	npSocket.sendNumpy(frame)
 
-
+try:
+    npSocket.endServer()
+except OSError as err:
+    print("error")
