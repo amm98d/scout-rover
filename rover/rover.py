ea3ddd1042c0152618f9940583687c19acdabfe2
amm98d <ahmedmustafamalik98@gmail.com>
2020-12-18 03:13:14 +0500

    [Refactor][Rover] Remove code for abandoned NumpySockets Streaming Approach.

--- rover/rover.py
+++ rover/rover.py
@@ -6,15 +6,12 @@ import serial
 import os
 import time
 import numpy as np
-from freenect import sync_get_depth as get_depth, sync_get_video as get_video
 import threading
 
 # internal modules
 import sys
 sys.path.append("/etc/scout/scout-rover/common/")
-from MovementMessage import *
 from NetworkHandler import *
-from NumpySocket import NumpySocket
 
 class Rover:
     """
@@ -36,28 +33,10 @@ class Rover:
             self.rover_socket.connect((ip,port))
 
             self.messagesListenerThread = threading.Thread(target=self.handle_incoming_messages)
-            self.sendStreamThread = threading.Thread(target=self.sendStream)
-
             self.messagesListenerThread.start()
-            self.sendStreamThread.start()
-
             self.messagesListenerThread.join()
-            self.sendStreamThread.join()
         finally:
             self.cleanUp()
-
-    def sendStream(self):
-        host_ip = '192.168.100.14'
-        npSocket = NumpySocket()
-        npSocket.startServer(host_ip, 9999)
-        while True:
-            frame, _ = get_video()
-            time.sleep(0.016)
-            npSocket.sendNumpy(frame)
-        try:
-            npSocket.endServer()
-        except OSError as err:
-            print("error")
 
     def handle_incoming_messages(self):
         """
@@ -75,11 +54,13 @@ class Rover:
         """
             Closes Connections.
         """
-        print("Cleaning Up")
-        if self.rover_socket is not None:
-            self.rover_socket.close()
-        if self.bluetooth_port is not None:
-            self.bluetooth_port.close()
+        try:
+            if self.rover_socket is not None:
+                self.rover_socket.close()
+            if self.bluetooth_port is not None:
+                self.bluetooth_port.close()
+        except Exception as e:
+            print(e)
 
 rover = Rover()
 rover.start()
