b3af206f29430d8daaa8593714f2e94a4499647d
Ahmed Mustafa Malik <ahmedmustafamalik98@gmail.com>
2020-12-18 01:09:36 +0500

    [Bug Fix][Server] Non-blocking keyboard input for controlling rover.

--- server/server.py
+++ server/server.py
@@ -6,13 +6,21 @@ import os
 from platform import platform
 import numpy as np
 import cv2
+import urllib.request
+import fpstimer
+import glob
 
 # internal modules
 import sys
 sys.path.append("../common/")
-from MovementMessage import *
 from NetworkHandler import *
-from NumpySocket import NumpySocket
+
+# from Map import Map
+# from Landmark import Landmark
+# from Robot import Robot
+# from Localization import Localization
+# from Odometry import *
+# from utils import *
 
 class Server:
     """
@@ -30,113 +38,202 @@ class Server:
                 - calls mainMenu()
         """
         try:
-            self.server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.server_socket.setblocking(True)
-            self.server_socket.bind(('',port))
-            self.server_socket.listen(1)
-            self.connection, self.client_address = self.server_socket.accept()
-
-            self.messagesListenerThread = threading.Thread(target=self.handle_incoming_messages)
-            self.receiveStreamThread = threading.Thread(target=self.receiveStream)
-            self.mainMenuThread = threading.Thread(target=self.mainMenu)
-
-            self.messagesListenerThread.start()
-            self.receiveStreamThread.start()
-            self.mainMenuThread.start()
-
-            self.mainMenuThread.join()
-            self.messagesListenerThread.join()
-            self.receiveStreamThread.join()
+            # self.server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            # self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            # self.server_socket.setblocking(True)
+            # self.server_socket.bind(('',port))
+            # self.server_socket.listen(1)
+            # self.connection, self.client_address = self.server_socket.accept()
+            self.mainMenu()
         finally:
-            self.cleanUp()
+            pass
+            # self.cleanUp()
 
-    def receiveStream(self):
-        npSocket = NumpySocket()
-        npSocket.startClient(9999)
-        while True:
-            frame = npSocket.recieveNumpy()
-            print(len(frame))
-            #cv2.imshow("The display window" , frame)
-        try:
-            npSocket.endServer()
-        except OSError as err:
-            print("error",err)
+    def _getFrame(self):
+        imgResp = urllib.request.urlopen('http://192.168.100.45:8080/shot.jpg')
+        imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
+        img = cv2.imdecode(imgNp, -1)
+        return img
 
-    def handle_incoming_messages(self):
-        """
-            Listens for messages from the Rover.
-        """
+    def localize(self):
+        # initialize frames
+        frame_A = self._getFrame()
+        frame_B = self._getFrame()
+        images = [frame_A, frame_B]
+
+        # initialize localization variables
+        ## GLOBAL VARIABLES
+        P = np.eye(4)  # Pose
+        k = np.array([[923.0, 0.0, 657.0],
+                [0.0, 923.0, 657.0],
+                [0.0000,   0.0000,   1.0000]], dtype=np.float32)
+        
+        poses = [[0.0, 0.0, 0.0]]  # initial pose
+        ## Create Map
+        env_map = Map(62, 62)
+        ##store landmarks
+        getLandmarks = True
+        if getLandmarks:
+            hamza_images = glob.glob("./train/*.jpg")
+            # 0->ecat
+            # 1->chemistry
+            # 2->physics
+            # 3->genis
+            crd = [(0, 0), (62, 0), (0, 62), (62, 62)]  # coordinates
+            i = 0
+            for img in hamza_images:
+                img = cv2.imread(img)
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                kp, des = extract_features(gray)
+                env_map.add_landmark(Landmark(crd[i][0], crd[i][1], kp, des))
+                i += 1
+        particle_filter = Localization(env_map)
+
+        # Setting loop for 10 FPS
+        # timer = fpstimer.FPSTimer(60)
+
+        i = 0
+        trajectory = [np.array([0, 0, 0])]
+        camPoses = []
+        # main loop
         while True:
-            message = NetworkHandler().receive(self.connection)
-            if message:
-                print(message)
+            # Part I. Features Extraction
+            kp_list, des_list = extract_features_dataset(images, extract_features)
+            # Part II. Feature Matching
+            matches = match_features_dataset(des_list, match_features)
+            # Set to True if you want to use filtered matches or False otherwise
+            is_main_filtered_m = False
+            if is_main_filtered_m:
+                filtered_matches = filter_matches_dataset(filter_matches_distance, matches)
+                matches = filtered_matches
+            # Part III. Trajectory Estimation
+            P, rmat, tvec = estimate_trajectory(estimate_motion, matches, kp_list, k, P)
+            if len(camPoses) == 0:
+                camPoses.append((rmat, tvec))
+                new_trajectory = P[:3, 3]
+                trajectory.append(new_trajectory)
+                # poses.extend(pose)
+                # trajectory.append(P[:3, 3])
             else:
-                return
+                prevRmat, prevTvec = camPoses[-1]
 
-    def clearScreen(self):
+                if not np.allclose(rmat, prevRmat) and not np.allclose(tvec, prevTvec):
+                    camPoses.append((rmat, tvec))
+                    new_trajectory = P[:3, 3]
+                    trajectory.append(new_trajectory)
+                    # poses.extend(pose)
+                    # trajectory.append(P[:3, 3])
+                else:
+                    print("frame filtered...")
+            # Part IV. Localize
+            # last_pose = poses[-1]
+            # second_last_pose = poses[-2]
+            # particle_filter.motion_update([second_last_pose, last_pose])
+            # particle_filter.measurement_update(images[1])
+            # particle_filter.sample_particles()
+            # Part V. Save Visualization plot
+            # particle_filter.plot_data(i)
+            i += 1
+            # Get New Frames
+            frame_A = np.copy(frame_B)
+            frame_B = self._getFrame()
+            images = [frame_A, frame_B]
+            # cv2.imshow('RGB',cv2.resize(frame_A, (640, 480)))
+
+            # FPS Settings
+            # timer.sleep()
+
+            # Temporary
+            q = cv2.waitKey(1)
+            if q == ord("q"):
+                break
+            # Save visualization frames
+            plt.cla()
+            npTraj = np.array(trajectory).T
+            visualize_trajectory(npTraj)
+            plt.savefig(f'dataviz/frame{i}.png')
+        cv2.destroyAllWindows()
+        # npSocket = NumpySocket()
+        # npSocket.startClient(9999)
+        # for i in range(10):
+        #     print(i)
+        #     frame = npSocket.recieveNumpy()
+        #     frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        #     cv2.imshow('RGB',frame)
+        #     print("received\n")
+        # try:
+        #     npSocket.endServer()
+        # except OSError as err:
+        #     print("error",err)
+
+    def _clearScreen(self):
+        print(platform())
         if platform() == "Windows":
             os.system("cls")
-        elif platform() == "Linux-5.4.0-52-generic-x86_64-with-Ubuntu-18.04-bionic":
+        elif platform() == "Linux-5.4.0-58-generic-x86_64-with-Ubuntu-18.04-bionic":
             os.system('clear')
 
-    def drivingMode(self):
-        def get_raw_char():
-            ch = ''
-            if os.name == 'nt':
-                import msvcrt
-                ch = msvcrt.getch()
-            else:
-                import tty, termios, sys
-                fd = sys.stdin.fileno()
-                old_settings = termios.tcgetattr(fd)
-                try:
-                    tty.setraw(sys.stdin.fileno())
-                    ch = sys.stdin.read(1)
-                finally:
-                    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-            return ch
-        self.clearScreen()
+    def initiateExploration(self):
+        self._clearScreen()
         print("===================================================================")
         print("                           Driving Mode                            ")
         print("===================================================================")
         print("Instructions:")
-        print("Use the arrow keys to drive the rover around. Press Esc Key to exit.")
+        print(" ==> Use the Arrow Keys to drive the rover around.")
+        print(" ==> Use Spacebar to break.")
+        print(" ==> Press Esc Key to exit.")
         print("-------------------------------------------------------------------")
-        while (True):
-            k = get_raw_char()
-            if ord(k) == 119:
-                NetworkHandler().send(b'w',self.connection)
-                print("up")
-            elif ord(k) == 115:
-                NetworkHandler().send(b's',self.connection)
-                print("down")
-            elif ord(k) == 100:
-                NetworkHandler().send(b'd',self.connection)
-                print("right")
-            elif ord(k) == 97:
-                NetworkHandler().send(b'a',self.connection)
-                print("left")
-            elif ord(k) == 32:
-                NetworkHandler().send(b' ',self.connection)
-                print('brakes')
-            elif ord(k) == 27:
-                print("Esc")
-                return
+        import sys
+        import select
+        import tty
+        import termios
+        def isData():
+            return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+        old_settings = termios.tcgetattr(sys.stdin)
+        try:
+            tty.setcbreak(sys.stdin.fileno())
+            while True:
+                # for manual non-blocking user controlled movement of rover
+                if isData():
+                    k = sys.stdin.read(1)
+                    if ord(k) == 119:
+                        # NetworkHandler().send(b'w',self.connection)
+                        print("up")
+                    elif ord(k) == 115:
+                        # NetworkHandler().send(b's',self.connection)
+                        print("down")
+                    elif ord(k) == 100:
+                        # NetworkHandler().send(b'd',self.connection)
+                        print("right")
+                    elif ord(k) == 97:
+                        # NetworkHandler().send(b'a',self.connection)
+                        print("left")
+                    elif ord(k) == 32:
+                        # NetworkHandler().send(b' ',self.connection)
+                        print('brakes')
+                    elif ord(k) == 27:
+                        print("Esc")
+                        return
+        finally:
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
     def mainMenu(self):
         while (True):
-            self.clearScreen()
+            self._clearScreen()
             print("=========================================")
             print("                Main Menu                ")
             print("=========================================")
-            print("1. Driving Mode.")
+            print("1. Initiate Exploration.")
             print("0. Exit.")
             print("-----------------------------------------")
             choice = input("Your Choice: ")
             if (choice=="1"):
-                self.drivingMode()
+                self.initiateExploration()
             elif (choice=="0"):
+                self._clearScreen()
+                print()
+                print("Scout-Rover signing off.")
+                print()
                 return
 
     def cleanUp(self):
