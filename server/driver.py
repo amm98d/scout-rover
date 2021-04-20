c9219be9a3def63ace9d3abd7343b7296f9b7ef5
amm98d <ahmedmustafamalik98@gmail.com>
2021-04-21 02:02:36 +0500

    [Feature][Server] Flask Integration. Initial Wrap.

--- server/driver.py
+++ server/driver.py
@@ -29,146 +29,147 @@ sys.path.append("./slam/")
 # from NetworkHandler import *
 from SLAM import *
 
-
 np.random.seed(1)
 
-#####################
-# READ FRAMES START
-#####################
-DATASET = 3
-NUM_FRAMES = -1
+class Driver:
+    def __init__(self):
+        #####################
+        # READ FRAMES START
+        #####################
+        self.DATASET = 0
+        self.NUM_FRAMES = -1
 
-metadata = {
-    0: {
-        'directory': os.path.join('datasets', 'carla'),
-        'depth': True,
-        'associate': False,
-        'depth_factor': 1,
-        'camera_matrix': [[640.0, 0, 640.0], [0, 480.0, 480.0], [0, 0, 1.0]],
-        'dist_coff': None,
-    },
-    1: {
-        'directory': os.path.join('datasets', 'fr1_xyz'),
-        'depth': True,
-        'associate': True,
-        'depth_factor': 5000,
-        'camera_matrix': [[525.0, 0, 319.5], [0, 525.0, 239.5], [0, 0, 1.0]],
-        'dist_coff': None, #[0.2624, -0.9531, -0.0054, 0.0026, 1.1633],
-    },
-    2: {
-        'directory': os.path.join('datasets', 'fr1_rpy'),
-        'depth': True,
-        'associate': True,
-        'depth_factor': 5000,
-        'camera_matrix': [[525.0, 0, 319.5], [0, 525.0, 239.5], [0, 0, 1.0]],
-        'dist_coff': None, #[0.2624, -0.9531, -0.0054, 0.0026, 1.1633],
-    },
-    3: {
-        'directory': os.path.join('datasets', 'fr2_pslam'),
-        'depth': True,
-        'associate': True,
-        'depth_factor': 5000,
-        'camera_matrix': [[525.0, 0, 319.5], [0, 525.0, 239.5], [0, 0, 1.0]],
-        'dist_coff': None, #[0.2312, -0.7849, -0.0033, -0.0001, 0.9172],
-    },
-    4: {
-        'directory': os.path.join('datasets', 'trajectory220'),
-        'depth': False,
-        'associate': False,
-        'depth_factor': 0,
-        'camera_matrix': [[827.0, 0, 638.0], [0, 826.0, 347.0], [0, 0, 1.0]],
-        'dist_coff': None,
-    },
-}
+        self.metadata = {
+            0: {
+                'directory': os.path.join('datasets', 'carla'),
+                'depth': True,
+                'associate': False,
+                'depth_factor': 1,
+                'camera_matrix': [[640.0, 0, 640.0], [0, 480.0, 480.0], [0, 0, 1.0]],
+                'dist_coff': None,
+            },
+            1: {
+                'directory': os.path.join('datasets', 'fr1_xyz'),
+                'depth': True,
+                'associate': True,
+                'depth_factor': 5000,
+                'camera_matrix': [[525.0, 0, 319.5], [0, 525.0, 239.5], [0, 0, 1.0]],
+                'dist_coff': None, #[0.2624, -0.9531, -0.0054, 0.0026, 1.1633],
+            },
+            2: {
+                'directory': os.path.join('datasets', 'fr1_rpy'),
+                'depth': True,
+                'associate': True,
+                'depth_factor': 5000,
+                'camera_matrix': [[525.0, 0, 319.5], [0, 525.0, 239.5], [0, 0, 1.0]],
+                'dist_coff': None, #[0.2624, -0.9531, -0.0054, 0.0026, 1.1633],
+            },
+            3: {
+                'directory': os.path.join('datasets', 'fr2_pslam'),
+                'depth': True,
+                'associate': True,
+                'depth_factor': 5000,
+                'camera_matrix': [[525.0, 0, 319.5], [0, 525.0, 239.5], [0, 0, 1.0]],
+                'dist_coff': None, #[0.2312, -0.7849, -0.0033, -0.0001, 0.9172],
+            },
+            4: {
+                'directory': os.path.join('datasets', 'trajectory220'),
+                'depth': False,
+                'associate': False,
+                'depth_factor': 0,
+                'camera_matrix': [[827.0, 0, 638.0], [0, 826.0, 347.0], [0, 0, 1.0]],
+                'dist_coff': None,
+            },
+        }
 
+    
+    def createFrameGenerator(self):
 
-def createFrameGenerator():
+        directory = self.metadata[self.DATASET]['directory']
+        hasDepth = self.metadata[self.DATASET]['directory']
+        needsAssociation = self.metadata[self.DATASET]['associate']
 
-    directory = metadata[DATASET]['directory']
-    hasDepth = metadata[DATASET]['directory']
-    needsAssociation = metadata[DATASET]['associate']
+        rgbDir = os.path.join(directory, 'rgb')
+        depthDir = os.path.join(directory, 'depth')
 
-    rgbDir = os.path.join(directory, 'rgb')
-    depthDir = os.path.join(directory, 'depth')
-
-    if needsAssociation:
-        associationFile = os.path.join(directory, 'associated.txt')
-        with open(associationFile, 'r') as inFile:
-            fileData = inFile.readlines()
-        for line in fileData:
-            line = line.split()
-            rgb = line[1].split('/')[-1]
-            depth = line[3].split('/')[-1]
-            rgbFile = os.path.join(rgbDir, rgb)
-            depthFile = os.path.join(depthDir, depth)
-            img = cv.imread(rgbFile, cv.IMREAD_UNCHANGED)
-            depth = cv.imread(depthFile, cv.IMREAD_UNCHANGED)
-            yield img, depth
-    else:
-        rgbFileNames = [os.path.join(rgbDir, fileName)
-                        for fileName in os.listdir(rgbDir)]
-        depthFileNames = [None for _ in range(len(rgbFileNames))]
-        if hasDepth:
-            depthFileNames = [
-                os.path.join(depthDir, fileName) for fileName in os.listdir(depthDir)
-            ]
-
-        for rgbFile, depthFile in zip(rgbFileNames, depthFileNames):
-            img = cv.imread(rgbFile, cv.IMREAD_UNCHANGED)
-            if hasDepth:
-                depth = np.loadtxt(depthFile, delimiter=",",
-                                   dtype=np.float64) * 1000.0
-                # depth = cv.imread(depthFile, cv.IMREAD_UNCHANGED)
+        if needsAssociation:
+            associationFile = os.path.join(directory, 'associated.txt')
+            with open(associationFile, 'r') as inFile:
+                fileData = inFile.readlines()
+            for line in fileData:
+                line = line.split()
+                rgb = line[1].split('/')[-1]
+                depth = line[3].split('/')[-1]
+                rgbFile = os.path.join(rgbDir, rgb)
+                depthFile = os.path.join(depthDir, depth)
+                img = cv.imread(rgbFile, cv.IMREAD_UNCHANGED)
+                depth = cv.imread(depthFile, cv.IMREAD_UNCHANGED)
                 yield img, depth
-            else:
-                yield img, -1
+        else:
+            rgbFileNames = [os.path.join(rgbDir, fileName)
+                            for fileName in os.listdir(rgbDir)]
+            depthFileNames = [None for _ in range(len(rgbFileNames))]
+            if hasDepth:
+                depthFileNames = [
+                    os.path.join(depthDir, fileName) for fileName in os.listdir(depthDir)
+                ]
+
+            for rgbFile, depthFile in zip(rgbFileNames, depthFileNames):
+                img = cv.imread(rgbFile, cv.IMREAD_UNCHANGED)
+                if hasDepth:
+                    depth = np.loadtxt(depthFile, delimiter=",",
+                                    dtype=np.float64) * 1000.0
+                    # depth = cv.imread(depthFile, cv.IMREAD_UNCHANGED)
+                    yield img, depth
+                else:
+                    yield img, -1
+
+    def doSlam(self):
+        FRAME_GENERATOR = self.createFrameGenerator()
 
 
-FRAME_GENERATOR = createFrameGenerator()
+        def getFrame():
+            for i in FRAME_GENERATOR:
+                return i
+
+            return -1, -1
 
 
-def getFrame():
-    for i in FRAME_GENERATOR:
-        return i
+        #####################
+        # READ FRAMES END
+        #####################
 
-    return -1, -1
+        # GLOBAL VARIABLES
+        # poseFig, poseAxis = plt.subplots()
+        depthFactor = self.metadata[self.DATASET]['depth_factor']
+        camera_matrix = self.metadata[self.DATASET]['camera_matrix']
+        dist_coff = self.metadata[self.DATASET]['dist_coff']
+        self.slamAlgorithm = SLAM(depthFactor, camera_matrix, dist_coff)
 
+        frameA, depthA = getFrame()
+        frameB, depthB = getFrame()
+        images = [frameA, frameB]
+        depths = [depthA, depthB]
 
-#####################
-# READ FRAMES END
-#####################
+        i = 2
+        while True:
+            # SLAMMING
+            if i > 1:
+                self.slamAlgorithm.process(images, depths, i)
+                # cv.waitKey(2000)
+            i += 1
 
-# GLOBAL VARIABLES
-# poseFig, poseAxis = plt.subplots()
-depthFactor = metadata[DATASET]['depth_factor']
-camera_matrix = metadata[DATASET]['camera_matrix']
-dist_coff = metadata[DATASET]['dist_coff']
-slamAlgorithm = SLAM(depthFactor, camera_matrix, dist_coff)
+            # Update Measurements
+            frameA = np.copy(frameB)
+            depthA = np.copy(depthB)
+            frameB, depthB = getFrame()
+            if np.isscalar(frameB) or i > 1000:
+                break
+            images = [frameA, frameB]
+            depths = [depthA, depthB]
 
-frameA, depthA = getFrame()
-frameB, depthB = getFrame()
-images = [frameA, frameB]
-depths = [depthA, depthB]
+        # trajectory = self.slamAlgorithm.get_trajectory()
+        # visualize_data(visualize_trajectory, True, True, "3D", trajectory)
 
-i = 2
-while True:
-    # SLAMMING
-    if i > 1:
-        slamAlgorithm.process(images, depths, i)
-        # cv.waitKey(2000)
-    i += 1
-
-    # Update Measurements
-    frameA = np.copy(frameB)
-    depthA = np.copy(depthB)
-    frameB, depthB = getFrame()
-    if np.isscalar(frameB) or i > 1000:
-        break
-    images = [frameA, frameB]
-    depths = [depthA, depthB]
-
-trajectory = slamAlgorithm.get_trajectory()
-visualize_data(visualize_trajectory, True, True, "3D", trajectory)
-
-poses = slamAlgorithm.get_robot_poses()
-visualize_data(plot_robot_poses, True, True, f"poses", poses)
+        # poses = self.slamAlgorithm.get_robot_poses()
+        # visualize_data(plot_robot_poses, True, True, f"poses", poses)
