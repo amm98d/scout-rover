77bb831e5e1479139e9f58f1ca568b254677e390
amm98d <ahmedmustafamalik98@gmail.com>
2021-04-21 20:45:45 +0500

    [Bug Fix][Server] Fix Double Threading Issue

--- server/app.py
+++ server/app.py
@@ -49,4 +49,4 @@ def video():
     return Response(genVideo(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='5000', debug=True)
+    app.run(host='0.0.0.0',port='5000')
